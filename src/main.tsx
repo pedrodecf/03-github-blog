bda7aacb291469578d36b9dddcc18cd6d2ded872
pedrodecf <contato.pedrodecf@gmail.com>
2024-01-16 22:38:51 -0300

    feat: add initial routes and initial page/home

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import { Routes } from './routes'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
@@ -8,7 +8,7 @@ import { GlobalStyle } from './styles/global'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <App />
+      <Routes />
       <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>,
