24e2fc19a469d65c8b432c5d47d6ae1c7fa0fd0c
pedrodecf <contato.pedrodecf@gmail.com>
2024-01-16 23:05:58 -0300

    chore: add DefaultLayout

---
+++ src/layouts/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`
+
+export const OutletContainer = styled.div`
+  max-width: 54rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Home } from '../pages/Home'
+import { DefaultLayout } from '../layouts'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
     </Routes>
   )
 }
