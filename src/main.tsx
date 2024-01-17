967fe89422e5e5f174b18e10aadcb28de022cf48
pedrodecf <contato.pedrodecf@gmail.com>
2024-01-16 22:27:56 -0300

    feat: add global styles and theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  background: '#071422',
+  border: '#1C2F41',
+  input: '#040F1A',
+  label: '#3A536B',
+  post: '#112131',
+  profile: '#0B1B2B',
+  span: '#7B96B2',
+  subtitle: '#C4D4E3',
+  text: '#AFC2D4',
+  title: '#E7EDF4',
+  blue: '#3294F8',
+}
