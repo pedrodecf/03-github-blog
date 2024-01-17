74d07976800028a049ca1255cda9b0c236fcba45
pedrodecf <contato.pedrodecf@gmail.com>
2024-01-17 18:07:25 -0300

    feat: add PostContext in routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { UserProvider } from './context/UserContext'
+import { PostProvider } from './context/PostContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
       <UserProvider>
-        <Routes />
+        <PostProvider>
+          <Routes />
+        </PostProvider>
       </UserProvider>
       <GlobalStyle />
     </ThemeProvider>
